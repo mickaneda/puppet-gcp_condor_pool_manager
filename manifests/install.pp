2a98c6b38f5834811beaaf8004957ae033194058
Michiru Kaneda <Michiru.Kaneda@gmail.com>
2018-09-24 12:43:36 +0900

    fixed install

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,5 +1,5 @@
 class gcp_condor_pool_manager::install {
   exec {"install gcp_condor_pool_manager":
-    command => "/usr/bin/curl -fsSL https://raw.github.com/mikaneda/gcp_condor_pool_manager/install/install.sh| sh"
+    command => "/usr/bin/curl -fsSL https://raw.githubusercontent.com/mickaneda/gcp_condor_pool_manager/install/install.sh|sh"
   }
 }

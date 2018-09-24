ea111a1f691ad7a0a0b35061ae982d409b69bfc4
Michiru Kaneda <Michiru.Kaneda@gmail.com>
2018-09-24 13:24:04 +0900

    fixed config

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,24 +1,24 @@
 class gcp_condor_pool_manager::config {
-  $core        = $gcp_condor_pool_manager::params::core
-  $mem         = $gcp_condor_pool_manager::params::mem
-  $disk        = $gcp_condor_pool_manager::params::disk
-  $max         = $gcp_condor_pool_manager::params::max
-  $max_cores   = $gcp_condor_pool_manager::params::max_cores
-  $static      = $gcp_condor_pool_manager::params::static
-  $prefix      = $gcp_condor_pool_manager::params::prefix
-  $image       = $gcp_condor_pool_manager::params::image
-  $preemptible = $gcp_condor_pool_manager::params::preemptible
-  $off_timer   = $gcp_condor_pool_manager::params::off_timer
-  $zones       = $gcp_condor_pool_manager::params::zones
-  $network_tag = $gcp_condor_pool_manager::params::network_tag
-  $reuse       = $gcp_condor_pool_manager::params::reuse
-  $interval    = $gcp_condor_pool_manager::params::interval
-  $head_info   = $gcp_condor_pool_manager::params::head_info
-  $bucket      = $gcp_condor_pool_manager::params::bucket
-  $admin       = $gcp_condor_pool_manager::params::admin
-  $owner       = $gcp_condor_pool_manager::params::owner
-  $log_level   = $gcp_condor_pool_manager::params::log_level
-  $gcloud      = $gcp_condor_pool_manager::params::gcloud
+  $core        = $gcp_condor_pool_manager::core
+  $mem         = $gcp_condor_pool_manager::mem
+  $disk        = $gcp_condor_pool_manager::disk
+  $max         = $gcp_condor_pool_manager::max
+  $max_cores   = $gcp_condor_pool_manager::max_cores
+  $static      = $gcp_condor_pool_manager::static
+  $prefix      = $gcp_condor_pool_manager::prefix
+  $image       = $gcp_condor_pool_manager::image
+  $preemptible = $gcp_condor_pool_manager::preemptible
+  $off_timer   = $gcp_condor_pool_manager::off_timer
+  $zones       = $gcp_condor_pool_manager::zones
+  $network_tag = $gcp_condor_pool_manager::network_tag
+  $reuse       = $gcp_condor_pool_manager::reuse
+  $interval    = $gcp_condor_pool_manager::interval
+  $head_info   = $gcp_condor_pool_manager::head_info
+  $bucket      = $gcp_condor_pool_manager::bucket
+  $admin       = $gcp_condor_pool_manager::admin
+  $owner       = $gcp_condor_pool_manager::owner
+  $log_level   = $gcp_condor_pool_manager::log_level
+  $gcloud      = $gcp_condor_pool_manager::gcloud
 
   file { '/etc/gcpm.conf':
     content => template("${module_name}/gcpm.conf.erb"),
